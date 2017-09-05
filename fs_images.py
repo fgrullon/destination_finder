c976127522055a54c2ddc9b8116484163d5dca5e
fgrullon <franfelix@gmail.com>
2017-09-05 16:51:12 -0400

    playing with fs api

--- fs_images.py
+++ fs_images.py
@@ -5,16 +5,22 @@ import requests
 class get_images:
 
   def get_img(self, lat, lng):
-  	location = "{0},{1}".format(lat, lng)
-  	ex = "https://api.foursquare.com/v2/venues/search?ll="+location+"&oauth_token=OWCWXKGK0CR1HSDG40Q4422IWUYB5PQW1YJKITQQJO5EBKE4&v=20170904"
-  	obj = urlopen(ex)
-  	data = json.load(obj)
-  	#venue_id = data["response"]["groups"][0]["items"][0]["venue"]["id"]
-  	venue_id = data["response"]["venues"][0]["categories"][0]["id"]
-  	#venue_name = data["response"]["groups"][0]["items"][0]["venue"]["name"]
-  	print("id : {}, name : ".format(venue_id))
-#https://api.foursquare.com/v2/venues/search?ll=37.783207,-122.441673&limit=50&categoryId=4d4b7105d754a06374d81259&intent=checkin&radius=520&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20120721
-  	"""
+    client_id = "WBAHTTWXR1GU0KIROJN1RT333EST1Q05TYHQ4XYB14VNO134"
+    client_secret = "OB4UGDBJ5JBTQKFKRKPCFFGSS5Z2EPTUO4POC41LWQYPEIF1"
+    location = "{0},{1}".format(lat, lng)
+    api = "https://api.foursquare.com/v2/venues/search?client_id="+client_id+"&client_secret="+client_secret+"&v=20130815&ll=18.48,-69.93"
+    ex = "https://api.foursquare.com/v2/venues/explore?ll="+location+"&oauth_token=OWCWXKGK0CR1HSDG40Q4422IWUYB5PQW1YJKITQQJO5EBKE4&v=20170904&radius=250"
+    query = "https://api.foursquare.com/v2/venues/explore?ll=18.4,-69&radius=250&oauth_token=OWCWXKGK0CR1HSDG40Q4422IWUYB5PQW1YJKITQQJO5EBKE4&v=20170905"
+    query_last = "https://api.foursquare.com/v2/venues/explore?ll=18.48,-69.93&radius=250&oauth_token=OWCWXKGK0CR1HSDG40Q4422IWUYB5PQW1YJKITQQJO5EBKE4&v=20170905"
+    obj = urlopen(api)
+    data = json.load(obj)
+    result = data["response"]["venues"]
+    for key in result:
+      print("{}".format(key))
+      #print("Result : {}\n".format(key["items"][i]["venue"]["name"]))
+
+    #key["items"][0]["venue"]["name"]
+"""
   	name = name.replace(" ", "")
   	#Get the venue id
   	query_id = "https://api.foursquare.com/v2/venues/search?query="+name+"&intent=global&oauth_token=OWCWXKGK0CR1HSDG40Q4422IWUYB5PQW1YJKITQQJO5EBKE4&v=20170904"
@@ -36,4 +42,4 @@ class get_images:
 
   	return arrer 
 
-  	   """ 
+"""
